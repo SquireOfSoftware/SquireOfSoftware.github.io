e9272b26c7369cecf4e0c83d94a7716fa53e62bc
Joseph Tran <fred.smoth@gmail.com>
2019-03-16 19:27:53 +1100

    Cleaning up the interface a lot
    - Updating the CSS to be a little bit more dynamic
    - Cleaning up the colour scheme so it looks a little bit more like the facebook chat

---
+++ js/initial.js
@@ -0,0 +1,6 @@
+// div id="chatbox"
+
+// document.getElementById("chatbox").scrollTop = document.getElementById("chatbox").scrollHeight;
+// document.getElementById("recent").scrollIntoView({behavior: "smooth"});
+window.scrollTo(0, document.getElementById("recent").scrollHeight);
+console.log("hello world");
