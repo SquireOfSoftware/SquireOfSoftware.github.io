d65dbfbf29c413ee841ffb26a2a9f142cbd87466
Joseph Tran <fred.smoth@gmail.com>
2021-10-23 11:45:31 +1100

    Adding some experimental pop ups
    - it is not working extremely cleanly, needs some more work in the CSS side to pop up from the node and pop back down to the node

--- timeline_cards/pages/cards/Card.js
+++ timeline_cards/pages/cards/Card.js
@@ -1,16 +1,102 @@
+import React, { useState, useRef } from "react";
 import styles from '../../styles/Card.module.css'
+import {motion, AnimatePresence} from 'framer-motion'
 
-export default function Card({title, image, blurb, link}) {
+// tutorial: https://www.geeksforgeeks.org/animated-expanding-card-using-framer-motion-and-reactjs/
+// seems to be out dated
+// recreate this https://www.framer.com/docs/animate-shared-layout/
+
+export default function Card(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [originalDimensions, setOriginalDimensions] = useState(null);
+  const toggleOpen = (event) => {
+    setIsOpen(!isOpen);
+    setOriginalDimensions(locateParentDiv(event.target));
+  }
+
+  function locateParentDiv(initialElement) {
+    // walk up the tree until you find the "overlay" element
+    let parent = initialElement.parentNode
+    while(parent.className !== undefined && parent.className.indexOf("overlay") === -1) {
+      parent = parent.parentNode;
+    }
+    console.log({parent})
+    return parent
+  }
+
   return (
     <>
-      <div className={styles.card}>
-        <img src={image} className={styles.card__img}/>
-        <div className={styles.card__body}>
-          <h2>{title}</h2>
-          <p>{blurb}</p>
-          <a href={link} target="_blank">Read More</a>
-        </div>
+      <div
+              style={{ pointerEvents: "auto", display: "inline-block" }}
+              className={styles.card + " overlay"}
+              onClick={toggleOpen}
+            >
+            <Content {...props} />
       </div>
+      <AnimatePresence>{isOpen && <FullScreenContent
+                                  closeHandler={() => setIsOpen(false)}
+                                  originalDimensions={originalDimensions}
+                                  {...props}/>
+                        }
+      </AnimatePresence>
     </>
   )
+}
+
+function Summary(props) {
+  return (
+    <div>Hello world</div>
+  )
+}
+
+function Content({title, image, blurb, link}) {
+  return (<>
+            <img src={image} className={styles.card__img}/>
+            <div className={styles.card__body}>
+              <h2>{title}</h2>
+              <p>{blurb}</p>
+              <a href={link} target="_blank">Read More</a>
+            </div>
+          </>)
+}
+
+function FullScreenContent({title, image, blurb, link, closeHandler, originalDimensions}) {
+  const originalDiv = {
+                          originX: originalDimensions.offsetLeft,
+                          originY: originalDimensions.offsetTop,
+                          height: originalDimensions.offsetHeight,
+                          width: originalDimensions.offsetWidth
+                        };
+  console.log({originalDiv})
+  return (<motion.div
+                  layout
+                  initial={{ opacity: 0,
+                             width: originalDiv.width,
+                             height: originalDiv.height,
+                             x: originalDiv.originX,
+                             y: originalDiv.originY,
+                             position: "absolute"}}
+                  animate={{ opacity: 1,
+                             width: "100%",
+                             height: "100%",
+                             position: "fixed",
+                             x: 0,
+                             top: 0}}
+                  exit={{ opacity: 0,
+                          width: originalDiv.width,
+                          height: originalDiv.height,
+                          x: originalDiv.originX,
+                          y: originalDiv.originY,
+                          position: "absolute"
+                           }}
+                  className={styles.card__fullscreen}
+                onClick={closeHandler}
+                >
+              <img src={image} className={styles.card__img_fullscreen}/>
+              <div className={styles.card__body}>
+                <h2>{title}</h2>
+                <p>{blurb}</p>
+                <a href={link} target="_blank">Read More</a>
+              </div>
+            </motion.div>)
 }
--- timeline_cards/pages/index.js
+++ timeline_cards/pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import LinkCard from './cards/LinkCard'
 import Card from './cards/Card'
+import {AnimateSharedLayout, motion} from 'framer-motion'
 
 export default function Home() {
   return (
@@ -22,28 +23,33 @@ export default function Home() {
           My name is Joseph TRAN and I am a software engineer.
         </p>
 
-        <div className={styles.grid}>
-          <Card link="https://github.com/SquireOfSoftware"
-                title="Main Github page"
-                blurb="For seeing what I have done outside of work."
-                image="./stars_night_sky_base.jpeg"/>
+        <div>
+          <AnimateSharedLayout>
+            <motion.div layout className={styles.grid}>
+              <Card link="https://github.com/SquireOfSoftware"
+                    title="Main Github page"
+                    blurb="For seeing what I have done outside of work."
+                    image="./stars_night_sky_base.jpeg"/>
 
-          <LinkCard link="https://github.com/SquireOfSoftware/Orion"
-                    title="Orion"
-                    blurb="An Autonomous Drone Flight system. Aiming to make drone flight autonomous via just highlighting key checkpoints on an iPad"/>
+              <Card link="https://github.com/SquireOfSoftware/Orion"
+                        title="Orion"
+                        blurb="An Autonomous Drone Flight system. Aiming to make drone flight autonomous via just highlighting key checkpoints on an iPad"/>
 
-          <LinkCard link="https://squireofsoftware.github.io/JSPets/"
-                    title="JSPets"
-                    blurb="A digital pet experience, walking around Tasmania"/>
+              <Card link="https://squireofsoftware.github.io/JSPets/"
+                        title="JSPets"
+                        blurb="A digital pet experience, walking around Tasmania"/>
 
-          <LinkCard link="https://github.com/SquireOfSoftware/NNFL/tree/master/Project/"
-                    title="Hand Gesture Music Player"
-                    blurb="Controlling your music player via hand gestures in Matlab with a Neural Network behind it"/>
+              <Card link="https://github.com/SquireOfSoftware/NNFL/tree/master/Project/"
+                        title="Hand Gesture Music Player"
+                        blurb="Controlling your music player via hand gestures in Matlab with a Neural Network behind it"/>
 
-          <LinkCard link="https://github.com/SquireOfSoftware/check-in"
-                    title="Check in"
-                    blurb="A sign in system to track the people who enter a church for the purposes of resource provisioning and forecasting for events and food"/>
+              <Card link="https://github.com/SquireOfSoftware/check-in"
+                        title="Check in"
+                        blurb="A sign in system to track the people who enter a church for the purposes of resource provisioning and forecasting for events and food"/>
+            </motion.div>
+          </AnimateSharedLayout>
         </div>
+
       </main>
 
       <footer className={styles.footer}>
